39a9bf571cbcb6547ea91e84ef68a6a955e75bf8
shino <sc@shinomin.me>
2023-03-29 18:48:17 +0700

    all: add build_inventory

--- cyberark.py
+++ cyberark.py
@@ -1,24 +1,11 @@
 import json
 import logging
-import os
-import requests
 
-base_url = ""
-session = requests.Session()
-session_token = ""
 requests_timeout_seconds = 30
+logger = logging.getLogger(__name__)
 
 
-def make_cyberark_requests(cyberark_base_url, cyberark_user, cyberark_pass):
-    global base_url
-    base_url = cyberark_base_url
-    global session_token
-    session_token = cyberark_logon(cyberark_user, cyberark_pass)
-
-    return
-
-
-def cyberark_logon(cyberark_user, cyberark_pass):
+def cyberark_logon(session, base_url, cyberark_user, cyberark_pass):
     url = f"{base_url}/PasswordVault/WebServices/auth/Cyberark/CyberArkAuthenticationService.svc/Logon"
     headers = {"Content-Type": "application/json"}
     payload = {"username": cyberark_user, "password": cyberark_pass}
@@ -33,34 +20,44 @@ def cyberark_logon(cyberark_user, cyberark_pass):
     ).json()
 
     session_token = json_response["CyberArkLogonResult"]
+    logger.debug(json_response)
+    logger.debug(f"session token: {session_token}")
 
     return session_token
 
 
-def cyberark_get_account_id(ip, os_user):
-    url = f"{base_url}/PasswordVault/api/accounts?search={ip},{os_user}"
-    headers = {"Content-Type": "application/json", "Authorization": session_token}
+def cyberark_get_account_id(session, session_token, base_url, ip, os_user):
+    # url = f"{base_url}/PasswordVault/api/accounts?search={ip},{os_user}"
+    # headers = {"Content-Type": "application/json", "Authorization": session_token}
 
-    json_response = session.get(
-        url=url, headers=headers, verify=False, timeout=requests_timeout_seconds
-    ).json()
+    # json_response = session.get(
+    #     url=url, headers=headers, verify=False, timeout=requests_timeout_seconds
+    # ).json()
 
-    account_id = json_response["value"][0]["id"]
+    # account_id = json_response["value"][0]["id"]
+    # logger.debug(json_response)
+    account_id = "123456"
+    logger.info(f"found account id {account_id} for {ip} {os_user}")
 
     return account_id
 
 
-def cyberark_get_password(account_id, reason):
-    url = f"{base_url}/PasswordVault/api/Accounts/{account_id}/password/retrieve"
-    headers = {"Content-Type": "application/json", "Authorization": session_token}
-    payload = {"actionType": "Show", "reason": reason}
-    json_payload = json.dumps(payload)
-    response = session.get(
-        url=url,
-        headers=headers,
-        data=json_payload,
-        verify=False,
-        timeout=requests_timeout_seconds,
-    )
+def cyberark_get_password(session, session_token, base_url, account_id, reason):
+    # url = f"{base_url}/PasswordVault/api/Accounts/{account_id}/password/retrieve"
+    # headers = {"Content-Type": "application/json", "Authorization": session_token}
+    # payload = {"actionType": "Show", "reason": reason}
+    # json_payload = json.dumps(payload)
 
-    return response.text
+    # response = session.get(
+    #     url=url,
+    #     headers=headers,
+    #     data=json_payload,
+    #     verify=False,
+    #     timeout=requests_timeout_seconds,
+    # )
+
+    # logger.debug(response)
+
+    # return response.text
+    response = "P@ssw0rdzz"
+    return response
--- inventory.py
+++ inventory.py
@@ -2,6 +2,10 @@ import logging
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 from ansible.vars.manager import VariableManager
+from cyberark import *
+import requests
+import os
+# import configparser
 
 logger = logging.getLogger(__name__)
 
@@ -35,3 +39,63 @@ def load_inventory(inventory_file):
     logger.info(inventory)
 
     return inventory
+
+
+def build_inventory(inventory_file, cyberark_base_url, cyberark_user, cyberark_pass, cyberark_request_reason
+):
+    session = requests.Session()
+    session_token = "some-token"
+    # session_token = cyberark_logon(
+    #     session=session,
+    #     base_url=cyberark_base_url,
+    #     cyberark_user=cyberark_user,
+    #     cyberark_pass=cyberark_pass,
+    # )
+
+    inventory = load_inventory(inventory_file)
+
+    for item in inventory:
+        logger.info(item)
+        account_id = cyberark_get_account_id(
+            session=session,
+            session_token=session_token,
+            base_url=cyberark_base_url,
+            ip=item["ip"],
+            os_user=item["user"],
+        )
+        password = cyberark_get_password(
+            session=session,
+            session_token=session_token,
+            base_url=cyberark_base_url,
+            account_id=account_id,
+            reason=cyberark_request_reason,
+        )
+
+        item.update({"password": password})
+
+    logger.debug(inventory)
+    update_password_to_ini_inventory(inventory=inventory, inventory_file=inventory_file)
+
+    return
+
+
+def update_password_to_ini_inventory(inventory, inventory_file):
+    inventory_lines = []
+    with open(inventory_file, "r") as file:
+        inventory_lines = [line.strip() for line in file]
+    file.close()
+
+    for idx, line in enumerate(inventory_lines):
+        for item in inventory:
+            if item["ip"] in line:
+                inventory_lines[idx] = f"{line} ansible_password=\"{item['password']}\""
+
+    logger.debug(inventory_lines)
+    os.remove(inventory_file)
+
+    with open(inventory_file, "w") as file:
+        for line in inventory_lines:
+            file.write(line + "\n")
+    file.close()
+
+    return
