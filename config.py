e5fad9f7b26f2dec0f1d12f8f232d9848c36b85d
shino <sc@shinomin.me>
2023-04-02 17:35:10 +0700

    all: use environs for reading env vars

--- config.py
+++ config.py
@@ -1,19 +1,24 @@
 import logging
-import os
+from environs import Env
 
 
 logger = logging.getLogger(__name__)
 
 
 def load_env():
+    env = Env()
+    env.read_env()
+
     envs = {}
 
-    envs["cyberark_user"] = os.environ.get("CYBERARK_USER")
-    envs["cyberark_pass"] = os.environ.get("CYBERARK_PASS")
-    envs["cyberark_base_url"] = os.environ.get("CYBERARK_URL")
-    envs["cyberark_request_reason"] = os.environ.get("CYBERARK_REASON")
+    envs["cyberark_user"] = env("CYBERARK_USER")
+    envs["cyberark_pass"] = env("CYBERARK_PASS")
+    envs["cyberark_base_url"] = env("CYBERARK_URL")
+    envs["cyberark_request_reason"] = env("CYBERARK_REASON")
 
-    envs["ansible_inventory"] = os.environ.get("ANSIBLE_INVENTORY")
+    envs["ansible_inventory"] = env("ANSIBLE_INVENTORY")
+    envs["ansible_use_vault"] = env.bool("ANSIBLE_USE_VAULT", True)
+    envs["ansible_vault_pass"] = env("ANSIBLE_VAULT_PASS")
 
     logger.info(envs)
     return envs
