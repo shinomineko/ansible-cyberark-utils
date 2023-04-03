01cb308afd43df99afd049bfefdd097e1f226728
Suracha Chowkuntod <sc@shinomin.me>
2023-04-03 16:21:31 +0700

    all: refactor tests

--- test_cyberark.py
+++ test_cyberark.py
@@ -6,44 +6,45 @@ from cyberark import *
 
 def test_cyberark_logon(requests_mock):
     base_url = load_env()["cyberark_base_url"]
+    want = "some-session-token"
     requests_mock.post(
         f"{base_url}/PasswordVault/WebServices/auth/Cyberark/CyberArkAuthenticationService.svc/Logon",
-        json={"CyberArkLogonResult": "some-session-token"},
+        json={"CyberArkLogonResult": want},
     )
 
-    session_token = cyberark_logon(requests.Session(), base_url, "user", "pass")
+    got = cyberark_logon(requests.Session(), base_url, "user", "pass")
 
-    assert session_token == "some-session-token"
+    assert got == want
 
 
 def test_cyberark_get_account_id(requests_mock):
     base_url = load_env()["cyberark_base_url"]
     ip = "192.168.2.2"
     user = "ansible"
+    want = "123abc"
     requests_mock.get(
         f"{base_url}/PasswordVault/api/accounts?search={ip},{user}",
-        json={
-            "value": [{"id": "123abc", "name": "some-name", "safeName": "some-safe"}]
-        },
+        json={"value": [{"id": want, "name": "some-name", "safeName": "some-safe"}]},
     )
 
-    account_id = cyberark_get_account_id(
+    got = cyberark_get_account_id(
         requests.Session(), "some-session-token", base_url, ip, user
     )
-    assert account_id == "123abc"
+    assert got == want
 
 
 def test_cyberark_get_password(requests_mock):
     base_url = load_env()["cyberark_base_url"]
     reason = "running test"
     account_id = "123abc"
+    want = "P@ssw0rdzz"
     requests_mock.get(
         f"{base_url}/PasswordVault/api/Accounts/{account_id}/password/retrieve",
-        text="P@ssw0rdzz",
+        text=want,
     )
 
-    password = cyberark_get_password(
+    got = cyberark_get_password(
         requests.Session(), "some-session-token", base_url, account_id, reason
     )
 
-    assert password == "P@ssw0rdzz"
+    assert got == want
