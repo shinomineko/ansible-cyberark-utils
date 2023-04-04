3cfc163dabb8e51c0f83023c9ad9dddbfaefa5bd
shino <sc@shinomin.me>
2023-04-04 12:21:37 +0700

    docker: add Dockerfile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
 import logging
+import textwrap
 
 from environs import Env
 
@@ -22,6 +24,22 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.RawTextHelpFormatter)
+    help_msg = textwrap.dedent("""
+required environment variables:
+ANSIBLE_INVENTORY    path to ansible inventory file
+ANSIBLE_USE_VAULT    use ansible vault to encrypt passwords. if ANSIBLE_USE_VAULT='false', the passwords will be added to ANSIBLE_INVENTORY (defaults to 'true')
+CYBERARK_URL         base url of CyberArk
+CYBERARK_USER        CyberArk username
+CYBERARK_PASS        CyberArk password
+CYBERARK_REASON      reason for retrieving passwords
+
+optional environment variables:
+ANSIBLE_VAULT_PASS   password for ansible vault. requires when ANSIBLE_USE_VAULT='true'
+    """)
+    parser.add_argument("-h", "--help", action="help", help=help_msg)
+    parser.parse_args()
+
     envs = load_env()
     cyberark_base_url = envs["cyberark_base_url"]
     cyberark_user = envs["cyberark_user"]
