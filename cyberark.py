a0a792f96ec1be75c3bd0dbfab428d54d72a15d5
shino <sc@shinomin.me>
2023-04-21 11:44:25 +0700

    cyberark: add logoff

--- cyberark.py
+++ cyberark.py
@@ -2,6 +2,8 @@ import json
 import logging
 
 requests_timeout_seconds = 30
+requests_tls_verify = False
+
 logger = logging.getLogger(__name__)
 
 
@@ -15,7 +17,7 @@ def cyberark_logon(session, base_url, cyberark_user, cyberark_pass):
         url=url,
         headers=headers,
         data=json_payload,
-        verify=False,
+        verify=requests_tls_verify,
         timeout=requests_timeout_seconds,
     ).json()
 
@@ -31,7 +33,10 @@ def cyberark_get_account_id(session, session_token, base_url, ip, os_user):
     headers = {"Content-Type": "application/json", "Authorization": session_token}
 
     json_response = session.get(
-        url=url, headers=headers, verify=False, timeout=requests_timeout_seconds
+        url=url,
+        headers=headers,
+        verify=requests_tls_verify,
+        timeout=requests_timeout_seconds,
     ).json()
 
     account_id = json_response["value"][0]["id"]
@@ -51,10 +56,29 @@ def cyberark_get_password(session, session_token, base_url, account_id, reason):
         url=url,
         headers=headers,
         data=json_payload,
-        verify=False,
+        verify=requests_tls_verify,
         timeout=requests_timeout_seconds,
     )
 
     logger.debug(response.json())
 
     return response.text.strip('"')
+
+
+def cyberark_logoff(session, session_token, base_url):
+    url = f"{base_url}/PasswordVault/api/auth/logoff"
+    headers = {"Content-Type": "application/json", "Authorization": session_token}
+    payload = {}
+    json_payload = json.dumps(payload)
+
+    json_response = session.post(
+        url=url,
+        headers=headers,
+        data=json_payload,
+        verify=requests_tls_verify,
+        timeout=requests_timeout_seconds,
+    ).json()
+
+    logger.debug(json_response)
+
+    return json_response
